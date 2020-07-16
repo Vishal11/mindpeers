28b2f153cd9c86a1b2aa0676b7a310402ef07024
gshakya30 <gshakya.singh007@gmail.com>
2020-07-17 01:48:45 +0530

    login

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -45,6 +45,56 @@ signupUser = function(user) {
     
 }
 
+authenticateUser = function(user) {
+
+    return new Promise((resolve, reject) => {
+
+        if(user.userType == 'doctor') {
+
+            bcrypt.hash(user.password, saltRounds, function(err, hash) {
+                if(err) {
+                    reject(err);
+                }
+                let pwd = hash;
+                DoctorSchema.findOne({email: user.email},function(err, data) {
+                    if(err) {
+                        reject(err);
+                    }
+                    console.log(data);
+                    if(!!data) {
+                        if(data.password == hash) {
+                            resolve("Successfull Login !!");
+                        } else {
+                            reject("Wrong Password !!")
+                        }
+                        
+                    } else{
+                        reject("User not exist !!")
+                    }
+                });
+            });
+    
+        } else {
+            const userModel = new UserSchema(user);
+            bcrypt.hash(user.password, saltRounds, function(err, hash) {
+                if(err) {
+                    reject(err);
+                }
+                userModel.password = hash;
+                userModel.save(function(err, data) {
+                    if(err) {
+                        reject(err);
+                    }
+                    resolve('success');
+                });
+            });
+    
+        }
+    })
+    
+}
+
 module.exports ={
-    signupUser
+    signupUser,
+    authenticateUser
 }
