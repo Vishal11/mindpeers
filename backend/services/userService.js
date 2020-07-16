c0016648dc1c2c63175f0c16b0788c99db0c29a6
gshakya30 <gshakya.singh007@gmail.com>
2020-07-17 01:04:20 +0530

    usertype

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,4 +1,6 @@
 var {UserSchema}= require('./../model/user');
+var {DoctorSchema}= require('./../model/doctor');
+
 var bcrypt = require('bcrypt');
 const saltRounds = 10;
 
@@ -7,21 +9,38 @@ signupUser = function(user) {
 
     return new Promise((resolve, reject) => {
 
-        const userModel = new UserSchema(user);
+        if(user.userType == 'doctor') {
+            const doctorModel = new DoctorSchema(user);
 
-        bcrypt.hash(user.password, saltRounds, function(err, hash) {
-            if(err) {
-                reject(err);
-            }
-            userModel.password = hash;
-            userModel.save(function(err, data) {
+            bcrypt.hash(user.password, saltRounds, function(err, hash) {
                 if(err) {
                     reject(err);
                 }
-                resolve('success');
+                doctorModel.password = hash;
+                doctorModel.save(function(err, data) {
+                    if(err) {
+                        reject(err);
+                    }
+                    resolve('success');
+                });
             });
-        });
-
+    
+        } else {
+            const userModel = new UserSchema(user);
+            bcrypt.hash(user.password, saltRounds, function(err, hash) {
+                if(err) {
+                    reject(err);
+                }
+                userModel.password = hash;
+                userModel.save(function(err, data) {
+                    if(err) {
+                        reject(err);
+                    }
+                    resolve('success');
+                });
+            });
+    
+        }
     })
     
 }
